60eb5214c7d01c9e669de75ec76ea3d9a7f488b0
Yago Ananias <yago_souza92@yahoo.com.br>
2021-02-07 12:35:33 -0300

    Var and Const

--- first_file.js
+++ first_file.js
@@ -1,1 +1,4 @@
 console.log("Fist Typescript Code");
+console.log("============");
+var nome = "Yago";
+console.log("Hi " + nome + ". " + "Welcome!");
--- first_file.ts
+++ first_file.ts
@@ -1,1 +1,7 @@
 console.log("Fist Typescript Code");
+
+console.log("============");
+
+const nome = "Yago";
+
+console.log("Hi " + nome + ". " + "Welcome!");
