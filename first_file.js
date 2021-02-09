10d69fec42060971fcd1e04247c6aeb8f762569b
Yago Ananias <yago_souza92@yahoo.com.br>
2021-02-08 23:44:43 -0300

    Código JS gerado

--- first_file.js
+++ first_file.js
@@ -2,3 +2,10 @@ console.log("Fist Typescript Code");
 console.log("============");
 var nome = "Yago";
 console.log("Hi " + nome + ". " + "Welcome!");
+var Produto = /** @class */ (function () {
+    function Produto(produtoNome, produtoValor) {
+        this.nome = produtoNome;
+        this.valor = produtoValor;
+    }
+    return Produto;
+}());
