e4d0407a93742acaa89401d8c808c35d37d43ca3
Yago Ananias <yago_souza92@yahoo.com.br>
2021-02-08 23:43:41 -0300

    Criando classe para converter em código JS

--- first_file.ts
+++ first_file.ts
@@ -5,3 +5,12 @@ console.log("============");
 const nome = "Yago";
 
 console.log("Hi " + nome + ". " + "Welcome!");
+
+class Produto {
+  nome: string;
+  valor: number;
+  constructor(produtoNome, produtoValor) {
+    this.nome = produtoNome;
+    this.valor = produtoValor;
+  }
+}
