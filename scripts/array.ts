68660627cd33a69c26dbeac1a9af50bde4bb6f5b
Yago Ananias <yago_souza92@yahoo.com.br>
2021-02-16 07:41:16 -0300

    Tuple

--- scripts/array.ts
+++ scripts/array.ts
@@ -1,11 +1,11 @@
 //Array somente de strings
-const filmesCategorias: string[] = ["Comédia", "Drama", "Romance", "Aventura"];
+//const filmesCategorias: string[] = ["Comédia", "Drama", "Romance", "Aventura"];
 
 //Array de strings OU de numeros
-const filmesCategorias: string[] | number[] = ["Comédia", "Drama", "Romance", "Aventura"];
+//const filmesCategorias: string[] | number[] = ["Comédia", "Drama", "Romance", "Aventura"];
 
 //Array de string E de numeros
-const filmesCategorias: (string | number)[] = ["Comédia", "Drama", "Romance", "Aventura", 10];
+//const filmesCategorias: (string | number)[] = ["Comédia", "Drama", "Romance", "Aventura", 10];
 
 //filmesCategorias.push("Ação");
 
---
+++ scripts/tuple.ts
@@ -0,0 +1,7 @@
+let superset: [string, number, string];
+
+superset = ["Typescript", 2011, "Anders"];
+
+let diasDaSemana: [string, string, string, string, string, string, string,];
+
+diasDaSemana = ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sáb"];
