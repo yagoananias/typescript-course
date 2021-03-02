4ddcd1d81ad7fc75baea4b834ad64517569f6507
Yago Ananias <yago_souza92@yahoo.com.br>
2021-03-02 07:56:18 -0300

    Interface

---
+++ scripts/interface.ts
@@ -0,0 +1,13 @@
+let endereco: {
+  logradouro: string,
+  numero: number,
+  bairro: string,
+  cidade: string
+};
+
+endereco = {
+  logradouro: "Rua dos bobos",
+  numero: 1000,
+  bairro: "Ipe",
+  cidade: "Bernô"
+}
