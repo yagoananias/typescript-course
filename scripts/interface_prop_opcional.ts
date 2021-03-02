c90cf6ea15863266d612b792f70d25b54bcd348d
Yago Ananias <yago_souza92@yahoo.com.br>
2021-03-02 09:45:06 -0300

    Exemplo de interface com opcional

--- scripts/interface_prop_opcional.ts
+++ scripts/interface_prop_opcional.ts
@@ -2,4 +2,12 @@ interface IProduto {
   nome: string;
   preco: number;
   descricao?: string;
+  dataValidade: Date;
+}
+
+const produtoDados: IProduto = {
+  nome: "Notebook",
+  preco: 7500,
+  descricao: "I7 10ª geração",
+  dataValidade: new Date(2022, 11, 5)
 }
