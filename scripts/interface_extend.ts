a9aa6edeb8ef5e6f7ee4496142baf381255e5c43
Yago Ananias <yago_souza92@yahoo.com.br>
2021-03-02 17:34:01 -0300

    Usuario Extends

--- scripts/interface_extend.ts
+++ scripts/interface_extend.ts
@@ -6,10 +6,20 @@ interface IModel {
 
 interface IPessoa extends IModel {
   nome: string;
-  idade: number; 
+  idade?: number; 
 }
 
 interface IUsuario extends IPessoa {
   email: string;
   senha: string;
+}
+
+const yago: IUsuario = {
+  id: 1,
+  created_at: new Date().getTime(),
+  updated_at: new Date().getTime(),
+  nome: "Yago",
+  idade: 28,
+  email: "contato@yagoananias.com.br",
+  senha: "root",
 }
