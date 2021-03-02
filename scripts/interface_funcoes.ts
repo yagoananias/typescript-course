bad550ef2ad63331b37ab77c3bc190c49f10516b
Yago Ananias <yago_souza92@yahoo.com.br>
2021-03-02 14:09:11 -0300

    Interface Métodos

--- scripts/interface_funcoes.ts
+++ scripts/interface_funcoes.ts
@@ -7,3 +7,27 @@ let primeiraSoma: ISoma;
 primeiraSoma = (numero1: number, numero2: number): number => {
   return numero1 + numero2;
 }
+
+interface ICalculos {
+  somar(a: number, b: number): number;
+  subtrair(a: number, b: number): number;
+  multiplicar(a: number, b: number): number;
+  dividir(a: number, b: number): number;
+}
+
+let calculadora: ICalculos;
+
+calculadora = {
+  dividir: (numero1: number, numero2: number) => {
+    return numero1 / numero2;
+  },
+  multiplicar: function (numero1: number, numero2: number) {
+    return numero1 * numero2;
+  },
+  somar: (numero1: number, numero2: number) => {
+    return numero1 + numero2;
+  },
+  subtrair: (numero1: number, numero2: number) => {
+    return numero1 - numero2;
+  }
+}
